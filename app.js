827eac3ceb74557239c6456eda318e682cdeeb65
Hafiz M. Usama <101815410+hafizusama747@users.noreply.github.com>
2023-11-11 06:15:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,8 @@ app.use(function(err, req, res, next) {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+    console.log("started");
+
 });
 
 module.exports = app;
